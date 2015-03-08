7d1658319c38e03eb956493903898c2ef485bb03
Derrell Lestyne <dlestyne@gmail.com>
2015-03-08 14:44:26 -0400

    Gear Update
    PLD - Fixed Gear and typos

--- Gearsets/Feary/PLD_Gearsets.lua
+++ Gearsets/Feary/PLD_Gearsets.lua
@@ -42,12 +42,12 @@ magicalshield = {sub="Aegis"}
 -- Standard/idle
 sets.idle.PDT = { ammo="Angha Gem",
 				head="Rev. Coronet +1", neck="Twilight Torque",
-                body="Cab. Surcoat +1", hands="Cizin Mufflers", lring=Aug.Darkring1, rring="Defending Ring",
+                body="Cab. Surcoat +1", hands="Cizin Mufflers +1", lring=Aug.Darkring1, rring="Defending Ring",
                 back="Repulse Mantle", waist="Flume Belt", legs="Cab. Breeches", feet="Rev. Leggings +1"}
 
 sets.idle.MDT = { ammo="Vanir Battery",
 				head="Rev. Coronet +1", neck="Twilight Torque", lear="Merman's Earring", rear="Merman's Earring",
-                body="Cab. Surcoat +1", hands="Cizin Mufflers", lring="Defending Ring", rring="Fortia Ring",
+                body="Cab. Surcoat +1", hands="Cizin Mufflers +1", lring="Defending Ring", rring="Fortia Ring",
                 back="Engulfer Cape", waist="Creed Baudrier", legs="Cab. Breeches", feet="Rev. Leggings +1"}
 
 sets.misc.Town = set_combine(sets.idle.PDT, {
@@ -58,7 +58,7 @@ sets.idle.Standard = set_combine(sets.idle.PDT, {
 -- TP 
 sets.TP = { 	ammo="Paeapua",
 				head="Otomi Helm", neck="Asperity Necklace", lear="Bladeborn Earring", rear="Steelflash Earring",
-                body="Pak Corselet", hands="Cizin Gauntlets", lring="Rajas Ring", rring="K'ayres Ring",
+                body="Pak Corselet", hands="Cizin Mufflers +1", lring="Rajas Ring", rring="K'ayres Ring",
                 back="Atheling Mantle", waist="Windbuffet Belt", legs="Cizin Breeches +1", feet="Ejekamal Boots"}
 sets.TP.Acc = { ammo="Ginsen",
 				head="Yaoyotl Helm", neck="Asperity Necklace", lear="Bladeborn Earring", rear="Steelflash Earring",
@@ -104,21 +104,21 @@ sets.midcast.EnhancingMagic = set_combine(sets.Recast,{})
 
 -- JA
 sets.precast.JA["Invincible"] = set_combine(sets.Enmity,{legs="Cab. Breeches"})
-sets.precast.JA["Intervene"] = set_combine(sets.precast.JA["Shield Bast"],{
+sets.precast.JA["Intervene"] = set_combine(sets.precast.JA["Shield Bash"],{
 				hands="Cab. Gauntlets +1",lear="Knightly Earring"})
 -- MND			
 sets.precast.JA["Chivalry"] = set_combine(sets.idle.PDT,{hands="Cab. Gauntlets +1"},{
 				head="Yaoyotl Helm", neck="Phalaina Locket",
-				body="Cab. Surcoat", lring="Aquasoul Ring", rring="Aquasoul Ring",
+				body="Cab. Surcoat +1", lring="Aquasoul Ring", rring="Aquasoul Ring",
 				legs="Cab. Breeches", feet="Whirlpool Greaves"})
 -- VIT
 sets.precast.JA["Rampart"] = set_combine(sets.Enmity,{
 				head="Cab. Coronet", 
-				body="Cab. Surcoat", hands="Cab. Gauntlets +1",
+				body="Cab. Surcoat +1", hands="Cab. Gauntlets +1",
 				waist="Chuq'aba Belt", legs="Cab. Breeches", feet="Whirlpool Greaves"})
 sets.precast.JA["Sentinel"] = set_combine(sets.Enmity,{feet="Cab. Leggings"})
 sets.precast.JA["Holy Circle"] = set_combine(sets.Enmity,{feet="Rev. Leggings +1"})
-sets.precast.JA["Fealty"] = set_combine(sets.Enmity,{body="Cab. Surcoat"})
+sets.precast.JA["Fealty"] = set_combine(sets.Enmity,{body="Cab. Surcoat +1"})
 sets.precast.JA["Shield Bash"] = set_combine(sets.Enmity,{
 				hands="Cab. Gauntlets +1",lear="Knightly Earring"})
 
@@ -132,7 +132,11 @@ sets.precast.WS['Requiescat'] = { ammo="",
                 body="Miki. Breastplate", hands="Miki. Gauntlets", lring="Aquasoul Ring", rring="Aquasoul Ring",
                 back="Atheling Mantle", waist="Fortia Belt", legs="Miki. Cuisses", feet="Whirlpool Greaves"}
 sets.precast.WS['Chant du Cygne'] = {ammo="",
-				head="Yaoyotl Helm", neck="Fortia Gorget", lear="Bladeborn Earring", rear="Steelflash Earring",
+				head="Yaoyotl Helm", neck="Fortia Gorget", lear="Brutal Earring", rear="Moonshade Earring",
+                body="Miki. Breastplate", hands="Miki. Gauntlets", lring="Rajas Ring", rring="Thundersoul Ring",
+                back="Atheling Mantle", waist="Wanion Belt",  legs="Miki. Cuisses", feet="Whirlpool Greaves"}
+sets.precast.WS['Savage Blade'] = {ammo="",
+				head="Yaoyotl Helm", neck="Fortia Gorget", lear="Brutal Earring", rear="Moonshade Earring",
                 body="Miki. Breastplate", hands="Miki. Gauntlets", lring="Rajas Ring", rring="Thundersoul Ring",
                 back="Atheling Mantle", waist="Wanion Belt",  legs="Miki. Cuisses", feet="Whirlpool Greaves"}
 sets.precast.WS['Atonement'] = set_combine(sets.precast.WS, sets.Enmity,{
